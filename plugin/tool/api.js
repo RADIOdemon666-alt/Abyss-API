04e9256649ad52fe80b0a0cb6cd5f29697665255
RADIOdemon666-alt <byzaryws@gmail.com>
2025-09-14 21:26:26 +0300

    Update api.js

--- plugin/tool/api.js
+++ plugin/tool/api.js
@@ -1,1 +1,13 @@
+export default (express) => {
+  const router = express.Router();
 
+  router.get("/", (req, res) => {
+    res.json({ message: "Hello from example API!" });
+  });
+
+  router.get("/test", (req, res) => {
+    res.json({ message: "Test route works!" });
+  });
+
+  return router;
+};
