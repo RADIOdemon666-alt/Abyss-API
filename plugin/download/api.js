b4124a4954a09db7d84a7aa62b0d66281967359f
RADIOdemon666-alt <byzaryws@gmail.com>
2025-09-14 23:51:29 +0300

    Update api.js

--- plugin/download/api.js
+++ plugin/download/api.js
@@ -1,11 +1,11 @@
 // plugin/download/api.js
-const express = require("express");
+const express = require('express');
 
 module.exports = () => {
   const router = express.Router();
 
-  router.get("/hello", (req, res) => {
-    res.json({ message: "Hello from Download API!" });
+  router.get('/hello', (req, res) => {
+    res.json({ message: 'Hello from Download API!' });
   });
 
   return router;
