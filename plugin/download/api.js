cf1e797d7355c5c0e60e2ef7ea19f4854078f3a8
RADIOdemon666-alt <byzaryws@gmail.com>
2025-09-16 22:09:14 +0300

    Delete plugin directory

--- plugin/download/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// مثال endpoint
-router.get('/hello', (req, res) => {
-  res.json({ message: "Hello from Download API!" });
-});
-
-module.exports = router;
